49a631ae48f6fff47077170c9c3875ccf44f5d34
MiltonAF <mafelizzolam@gmail.com>
2023-09-23 22:27:40 -0500

    actualizando

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import post
 
 # Create your views here.
+def render_post(request):
+    posts = post.objects.all()
+    return render(request,'post.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    p = get_object_or_404(post, pk=post_id)
+    return render(request, 'post_detail.html', {'post': p})
